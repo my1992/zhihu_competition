ad5c4679a74855d556821ffc701aea2c4d7c8579
my1992 <tjtjwmy@qq.com>
2017-05-17 17:08:38 +0800

    load word2vec model

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
-print('this is a test')
+from gensim.models import Word2Vec
+
+word_vectors = Word2Vec.load_word2vec_format('data/word_embedding.txt', binary=False)  # C text format
+print(list(word_vectors.vocab)[0:10])
